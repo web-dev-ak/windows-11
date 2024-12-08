8260930d8b03201334a91f33ebc38bf810b33691
web-dev-ak <141951920+web-dev-ak@users.noreply.github.com>
2024-12-08 18:58:04 +0530

    mobile nav update

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,15 @@
+// LIVE TIME AND DATE
 import { updateTimeWithSeconds, updateTimeWithoutSeconds, } from "./timeDate.js";
 import { getDateInDDMMYYYY, getDateInLongFormat } from "./timeDate.js";
 
 function updateTaskbarTime() {
     const timeTaskBar = document.getElementById('sys-info-time');
-    timeTaskBar.textContent = updateTimeWithSeconds(); // Get the current time with seconds
+    timeTaskBar.textContent = updateTimeWithSeconds();
 }
 
 function updateDesktopTime() {
     const timeDesktop = document.getElementById('desktop-time');
-    timeDesktop.textContent = updateTimeWithoutSeconds(); // Get the current time without seconds
+    timeDesktop.textContent = updateTimeWithoutSeconds();
 }
 
 setInterval(() => {
@@ -19,10 +20,8 @@ setInterval(() => {
 updateTaskbarTime();
 updateDesktopTime();
 
-
 const shortDateElement = document.getElementById('sys-info-date');
 const longDateElement = document.getElementById('desktop-date');
 
-// Example usage
-shortDateElement.textContent = getDateInDDMMYYYY(); // Outputs: 07-12-2024
-longDateElement.textContent = getDateInLongFormat(); // Outputs: Saturday, 7 December
+shortDateElement.textContent = getDateInDDMMYYYY();
+longDateElement.textContent = getDateInLongFormat();
